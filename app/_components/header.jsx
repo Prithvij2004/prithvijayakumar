8ddbb8f974859ca2d54f987ee952d66c2972e1ed
Prithvij2004 <manikandan2200j@gmail.com>
2025-01-18 14:58:44 +0530

    (refactor): page

--- app/_components/header.jsx
+++ app/_components/header.jsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default async function Header() {
   return (
-    <header className="flex justify-between items-center p-6 max-w-4xl mx-auto gap-x-72">
+    <header className="flex flex-col sm:flew-row justify-between items-center p-6 sm:p-6 max-w-4xl mx-auto space-y-4 sm:space-y-0">
       <div className="flex-1">
         <Avatar className="w-12 h-12">
           <AvatarImage src="/avatar_pixel.png" alt="avatar" />
@@ -13,7 +13,7 @@ export default async function Header() {
       <div className="flex-1 text-center">
         <span className="text-lg font-bold">Welcome</span>
       </div>
-      <div className="flex-1 flex justify-end">
+      <div className="flex-1 flex justify-end sm:justify-end">
         <a
           href="mailto:prithvijayakumardev@gmail.com"
           className="font-bold text-[#8d7054] underline hover:text-[#684423] whitespace-nowrap"
--- app/page.js
+++ app/page.js
@@ -5,12 +5,12 @@ import { VerticalLineContent } from "./_components/vertical-line-content";
 
 export default function Page() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen lg:px-56">
       <Header />
       <main className="flex-grow max-w-4xl mx-auto px-6 mt-20">
         <VerticalLineContent />
       </main>
       <Footer />
-    </>
+    </ div>
   );
 }
